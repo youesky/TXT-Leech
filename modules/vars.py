24e7821d4d33cfdac3f162533954b28aa00da7b1
youesky <fousiyatc@gmail.com>
2024-03-04 10:18:17 +0530

    update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,11 @@
+from os import environ
+
 api_id = "28929701"
 api_hash = "ca53fb1dbc7e1d76cdc4d56536d1a5f9"
-bot_token = "6893027676:AAEytAdJL-5uY_2LqeCVWA7ziogwPvxBZQw"
+bot_token = environ['BOT_TOKEN']
+
+'''from os import environ
+
+api_id = environ['API_ID']
+api_hash = environ['API_HASH']
+bot_token = environ['BOT_TOKEN']'''
